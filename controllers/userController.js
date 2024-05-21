a0b28214a436ebf7d904eb178770ca9821e9816b
DTSmith17 <153142161+DTSmith17@users.noreply.github.com>
2024-05-21 11:49:48 -0500

    functions to create and update thoughts

--- controllers/userController.js
+++ controllers/userController.js
@@ -27,5 +27,44 @@ const thoughtController = {
       return res.status(500).json(err);
     }
   },
+
+  
+  // Create thought
+  async createThought(req, res) {
+    try {
+      const thought = await Thought.create(req.body);
+
+      const user = await User.findByIdAndUpdate(
+        req.body.userId,
+        { $addToSet: { thoughts: thought._id } },
+        { runValidators: true, new: true }
+      );
+
+      return res.status(200).json({ thought, user });
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  },
+
+  // Update thought
+  async updateThought(req, res) {
+    try {
+      const thought = await Thought.findOneAndUpdate(
+        { _id: req.params.thoughtId },
+        { $set: req.body },
+        { runValidators: true, new: true }
+      );
+
+      if (!thought) {
+        return res.status(404).json({ message: "No thought with this id!" });
+      }
+
+      return res.status(200).json(thought);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  },
 };
 module.exports = thoughtController;
