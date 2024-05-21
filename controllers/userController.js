61b8f77076a9dee315f07b235a40dd0045e453c7
DTSmith17 <153142161+DTSmith17@users.noreply.github.com>
2024-05-21 11:32:46 -0500

    Added userController with functions to get single and multiple thoughts.

---
+++ controllers/userController.js
@@ -0,0 +1,31 @@
+const { User, Thought } = require("../models");
+
+// Get all thoughts
+const thoughtController = {
+  async getThoughts(req, res) {
+    try {
+      const thoughts = await Thought.find();
+      return res.status(200).json(thoughts);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  },
+
+  // Get single thought
+  async getThought(req, res) {
+    try {
+      const thought = await Thought.findOne({ _id: req.params.thoughtId });
+
+      if (!thought) {
+        return res.status(404).json({ message: "No thought with that ID" });
+      }
+
+      return res.status(200).json(thought);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  },
+};
+module.exports = thoughtController;
